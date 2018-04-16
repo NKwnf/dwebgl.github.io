9f0f8c6c0942758a66dcbb17803e7a75ed4ae509
Ubuntu <ubuntu@ip-172-31-63-200.ec2.internal>
2018-04-16 21:34:06 +0000

    ori

--- server/uniquemachine/clear_data.sql
+++
@@ -1,6 +0,0 @@
-delete from features;
-delete from pictures;
-delete from cookies;
-ALTER TABLE features AUTO_INCREMENT = 1;
-ALTER TABLE pictures AUTO_INCREMENT = 1;
-ALTER TABLE cookies AUTO_INCREMENT = 1;
--- server/uniquemachine/uniquemachine.wsgi
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(0, "/home/ubuntu/server/uniquemachine/")
-from uniquemachine_app import app as application
